b9e2347fccc4de7dbaadb9d6d0949dc1593ec807
Krystian Nowak <177132@o365.prz.edu.pl>
2025-10-21 07:59:17 +0000

    2_2.1

--- script.js
+++ script.js
@@ -6,17 +6,27 @@
   const answer = document.getElementById('answer')
 
   example.addEventListener("click", function () {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(array => {
-        console.log(array)
-        answer.innerHTML = JSON.stringify(array);
-      })
+    
   })
 
   cw1.addEventListener("click", function () {
-    //TODO
-  })
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(posts => {
+        let html = '';
+        posts.forEach(post => {
+          html += `
+            <div class="post">
+              <h3>${post.title}</h3>
+              <p>${post.body}</p>
+            </div>
+          `;
+        });
+        console.log(html);
+        answer.innerHTML = html;
+      });
+  });
+
 
   cw2.addEventListener("click", function () {
     //TODO
